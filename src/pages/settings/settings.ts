7833a1d738ad791f4ffaf1dd37f2afa9422b8aab
Mrpyro22 <gallade22@gmail.com>
2018-04-12 12:56:19 +1000

    theme work start

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -14,11 +14,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
+  theme: any = 1;
+  imgSrc: any = 'assets/imgs/pokeball.png';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  onChange(){
+    console.log('log')
+    if(this.theme == 1)
+      this.imgSrc = 'assets/imgs/pokeball.png';
+    else if(this.theme == 2)
+      this.imgSrc = 'assets/imgs/greatball.png';
+    else if(this.theme == 3)
+      this.imgSrc = 'assets/imgs/ultraball.png';
+    else if(this.theme == 4)
+      this.imgSrc = 'assets/imgs/masterball.png';
   }
 
 }
