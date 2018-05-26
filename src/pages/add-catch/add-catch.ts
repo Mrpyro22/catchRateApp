63e2dd76a6e521e1141f17a07769b7a5432b8db7
Mrpyro22 <gallade22@gmail.com>
2018-05-26 11:17:24 +1000

    image picker

--- src/pages/add-catch/add-catch.ts
+++ src/pages/add-catch/add-catch.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { AlertController } from 'ionic-angular';
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Base64 } from '@ionic-native/base64';
+
 
 @IonicPage()
 @Component({
@@ -21,7 +24,16 @@ export class AddCatchPage {
   className: any = 'circle';
   imgSrc: any = 'assets/imgs/pokeball.png';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private storage: Storage, private alertCtrl: AlertController) {
+  theme: any;
+
+  image: any;
+  imageStatus: any = 'Add an image';
+  options: any = {
+    quality: 100
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private storage: Storage, private alertCtrl: AlertController,
+    public loadingCtrl: LoadingController, private imagePicker: ImagePicker, private base64: Base64) {
   }
 
   ionViewDidLoad() {
@@ -51,13 +63,18 @@ export class AddCatchPage {
   }
 
   checkVariables(){
-    if(this.pokemonName && this.level && this.currentHP && this.attempts && this.status && this.game && this.date){
+    if(this.pokemonName && this.level && this.currentHP && this.attempts && this.status && this.game && this.date && this.image){
       this.closeModal();
     }
     else{
       //if any fields are invalid, call the function to display an alert saying so
       this.invalidVar();
     }
+  }
+
+  cancelCheck(){
+    this.pokemonName = '';
+    this.closeModal();
   }
 
   //function creates an alert that warns of un-filled data fields
@@ -75,8 +92,32 @@ export class AddCatchPage {
   //function to close the modal
   closeModal() {
     //when called, the ViewController dismisses the modal
-    let data = {pokemonName: this.pokemonName, level: this.level, currentHP: this.currentHP, attempts: this.attempts, stats: this.status, game: this.game, date: this.date}
+    let data = {pokemonName: this.pokemonName, level: this.level, currentHP: this.currentHP, attempts: this.attempts,
+    stats: this.status, game: this.game, date: this.date, image: this.image}
     this.viewCtrl.dismiss(data);
+  }
+
+  select(){
+    const loading = this.loadingCtrl.create({
+    content: 'Please wait...'
+    });
+
+    this.imagePicker.getPictures(this.options).then((results) => {
+
+      loading.present();
+      for (var i = 0; i < results.length; i++) {
+            this.base64.encodeFile(results[i]).then((base64File: string) => {
+              let base64Image = base64File;
+              this.image = base64Image;
+              this.imageStatus = 'Image Accepted'
+            }, (err) => {
+              console.log(err);
+            });
+      }
+      loading.dismiss();
+
+
+    }, (err) => { });
   }
 
 }
